f9da3cc3bde5091106be4c7014b4c4fc22732605
abdelkarim benchelef <benchelef.abdelkarim@sonelgaz-serv.dz>
2023-04-17 09:06:03 +0100

    local commit before pull

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,23 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export default function navbar() {
+export default function    Navbar() {
   return (
     <div>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Full stack application</a>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Full stack application</a>
                 <button 
-                class="navbar-toggler" 
+                className="navbar-toggler" 
                 type="button" 
                 data-bs-toggle="collapse" 
                 data-bs-target="#navbarSupportedContent" 
                 aria-controls="navbarSupportedContent" 
                 aria-expanded="false" 
-                aria-label="Toggle navigation"
+                aria-label="Toggle navigation" 
                 >
                  <span className="navbar-toggler-icon"></span> 
                 </button> 
-                <button className="btn btn-outline-light">Add User</button>      
+                <Link className="btn btn-outline-light" to='/adduser'>Add User</Link>      
             </div>
         </nav>
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,38 +1,61 @@
-import React from 'react'
+import React, { useEffect, useState } from "react"
+import axios from "axios"
 
-export default function home() {
+export default function Home() {
+
+  const [users, setUsers]=useState([])
+
+  useEffect(()=>{
+    loadUsers()
+  }, [])
+
+  const loadUsers = async () =>
+  {
+    console.log("code with karim")
+    const result = await axios.get("http://localhost:8080/users");
+    console.log('loadUsers result ====>', result.data );
+    setUsers(result.data);
+  }
+ 
   return (
-    <div classNameName="container">
+
+    <div className="container">
         <div className ="py-4 px-4">
         <table className="table border shadow">
-            <thead>
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">Username</th>
+              <th scope="col">Email</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {  
+              users.map((user, index) => (
+                
                 <tr>
-                <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
+                  <th scope="row" key={index}>{ index+1}</th>
+                  <td>{user.name}</td>
+                  <td>{user.username}</td>
+                  <td>{user.email}</td>
+                  <td>
+                    <button type="button" class="btn btn-primary mx-2">View</button>
+                    <button type="button" class="btn btn-outline-primary mx-2">Edit</button>
+                    <button type="button" class="btn btn-danger mx-2">Delete</button>
+
+
+                  </td>
                 </tr>
-            </thead>
-            <tbody>
-                <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-                </tr>
-                <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-                </tr>
-                <tr>
-                <th scope="row">3</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
-                </tr>
-            </tbody>
-            </table>
+
+              ))
+            }
+          
+          </tbody>
+
+        </table>
         </div>
     </div>
   )
